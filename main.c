77d45b7b213cffb9b1bdcdd171df086ca5bcf848
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-03-24 17:24:54 +0700

    feat: draw a red pixel in middle of the window

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 
 int rgb_to_int(double r, double g, double b)
 {
-    int color = 0;
+    int color;
+	
+	color = 0;
     color |= (int)(b * 255);
     color |= (int)(g * 255) << 8;
     color |= (int)(r * 255) << 16;
@@ -17,5 +19,15 @@ int	main()
 	mlx_ptr = mlx_init();
 	window_ptr = mlx_new_window(mlx_ptr, 500, 500, "hello");
 	mlx_pixel_put(mlx_ptr, window_ptr, 500/2, 500/2, rgb_to_int(1,0,0));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	mlx_loop(mlx_ptr);
 }
