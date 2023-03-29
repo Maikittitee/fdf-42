44016168494b3d81cb718ded34dd34a3d7c77bbf
ktunchar <maikittitee1615@gmail.com>
2023-03-30 01:52:05 +0700

    fix[bresenham]: fix infinity loop

--- fdf_draw.c
+++ fdf_draw.c
@@ -19,6 +19,17 @@ void	bresenham(t_magic *data, float x0, float y0, float x1, float y1)
 	float	step_y;
 	float	delta_x;
 	float	delta_y;
+	int	z;
+	int	color;
+
+	z = data->z_metric[(int)y0][(int)x0];
+
+	if (z == 0)
+		color = rgb_to_int(1,1,1);
+	else
+		color = rgb_to_int(1,0,0);
+
+
 
 	x0 *= data->zoom;
 	y0 *= data->zoom;
@@ -34,9 +45,8 @@ void	bresenham(t_magic *data, float x0, float y0, float x1, float y1)
 
 	while (x0 < x1 || y0 < y1)
 	{
-		mlx_pixel_put(data->mlx, data->win, x0, y0,rgb_to_int(1,1,1));
+		mlx_pixel_put(data->mlx, data->win, x0, y0,color);
 		printf("End point x0:%.2f, y0:%.2f\nStep x:%.2f, Step_y:%.2f\n",x0,y0,step_x, step_y);
-
 		x0 += step_x;
 		y0 += step_y;
 	}
