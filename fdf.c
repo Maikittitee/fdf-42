a08aefdce47a3472e3a6eb4105bc27eb72c850a9
ktunchar <maikittitee1615@gmail.com>
2023-05-19 17:47:21 +0700

    draw from metric (pixel -> window)

--- fdf.c
+++ fdf.c
@@ -3,21 +3,22 @@
 int	main(int argc, char **argv)
 {
 	t_map *map;
-	// t_fdf *fdf;
+	t_fdf *fdf;
 
 
 	map = malloc(sizeof(t_map));
-	// fdf->mlx = mlx_init();
-	// fdf->win = mlx_new_window(data->mlx,1600,1080,"ktunchar's FdF");
-	// data->zoom = 20;
+	fdf = malloc(sizeof(t_fdf));
+	fdf->mlx_p = mlx_init();
+	fdf->win_p = mlx_new_window(fdf->mlx_p,1600,1080,"ktunchar's FdF");
+	fdf->zoom = 20;
 	if (argc != 2)
 		return (1);
 	read_map(argv[1], map);
 	printf("data.width: %d , data.height: %d\n", map->width, map->height);
 	printf("############ = Z_Metric = ###########\n");
 	print_metric(map);
-	// draw_from_metric(data);
+	draw_from_metric(map, fdf);
 
-	// mlx_loop(data->mlx);
+	mlx_loop(fdf->mlx_p);
 	return (0);
 }
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 00:44:40 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/05/18 03:12:13 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/05/19 17:44:09 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 typedef struct s_fdf{
 	void	*mlx_p;
 	void	*win_p;
+	int		zoom;
 } t_fdf;
 
 typedef struct t_map{
@@ -31,12 +32,18 @@ typedef struct t_map{
 	int	height;
 } t_map;
 
+float   ft_max(float a, float b);
+float   ft_abs(float n);
+void    bresenham(t_map *map,t_fdf *fdf, float x0, float y0, float x1, float y1);
+void    draw_from_metric(t_map *map, t_fdf *fdf);
+size_t  count_word(char const *s, char c);
+void get_width(char *filename, t_map *map);
+void    get_height(char *filename, t_map *map);
 void    assign_z_metric(char *filename, t_map *map);
 int     read_map(char *filename, t_map *map);
 void    print_metric(t_map *map);
 void    rotateZ(float *x, float *y, float z, float theta);
 void    ft_double_free(char **s);
 int rgb_to_int(double r, double g, double b);
-size_t	count_word(char const *s, char c);
 
 #endif
