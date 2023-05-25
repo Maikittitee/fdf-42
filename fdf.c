6cc744a352d45b63597f1d8282ba13c5f747accd
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-05-26 03:42:59 +0700

    feat: ratio

--- fdf.c
+++ fdf.c
@@ -23,11 +23,9 @@ int	ft_exit(int keycode,t_fdf *fdf)
 int	main(int argc, char **argv)
 {
 	t_map *old_map_data;
-	t_map *new_map_data;
 	t_fdf *fdf;
 
 	old_map_data = malloc(sizeof(t_map));
-	new_map_data = malloc(sizeof(t_map));
 	fdf = malloc(sizeof(t_fdf));
 	fdf->mlx_p = mlx_init();
 	fdf->win_p = mlx_new_window(fdf->mlx_p,WIN_WIDTH, WIN_HEIGHT,"ktunchar's FdF");
@@ -36,20 +34,17 @@ int	main(int argc, char **argv)
 		return (1);
 	read_map(argv[1], old_map_data);
 	printf("data.width: %d , data.height: %d\n", old_map_data->width, old_map_data->height);
-	old_map_data->ratio = 20;
-	old_map_data->theta = 0.5;
+	old_map_data->theta = 0.8;
+	old_map_data->ratio = get_ratio(old_map_data);
+	printf("ratio: %f\n", old_map_data->ratio);
+	// old_map_data->ratio = 20;
 	assign_xy(old_map_data);
-	new_map_data->height = old_map_data->height; 
-	new_map_data->width = old_map_data->width;
-	new_map_data->ratio = old_map_data->ratio;
-	new_map_data->theta = old_map_data->theta;
-	assign_xy(new_map_data);
 	assign_z_color(argv[1], old_map_data);
-	apply_iso(old_map_data , new_map_data);
-	apply_start_pnt(new_map_data);
+	apply_iso(old_map_data);
+	apply_start_pnt(old_map_data);
 	// apply_difference(old_map_data, new_map_data);
-	print_map(new_map_data);
-	draw_from_metric(new_map_data, fdf);
+	print_map(old_map_data);
+	draw_from_metric(old_map_data, fdf);
 	mlx_put_image_to_window(fdf->mlx_p, fdf->win_p, fdf->img_p , 0, 0);
 	
 	mlx_hook(fdf->win_p, 17, 0, close_win, fdf);
