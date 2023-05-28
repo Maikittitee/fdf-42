0a4dd828eb41bec05669249797b7455dedc83455
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-05-28 07:57:31 +0700

    haha

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 20:55:24 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/05/28 07:32:35 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/05/28 07:54:59 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,6 @@ int	main(int argc, char **argv)
 	dprintf(1, "height:%d | width:%d", map_data->height, map_data->width);
 	ft_initialize(map_data, argv[1], 0.523599);
 	draw_from_metric(map_data, fdf);
-	ft_dda(fdf, (t_point){0, WIN_HEIGHT/2, 0, 0x0000FF}, (t_point){WIN_WIDTH, WIN_HEIGHT/2, 0, 0x0000FF});
-	ft_dda(fdf, (t_point){WIN_WIDTH/2, 0, 0, 0x0000FF}, (t_point){WIN_WIDTH/2, WIN_HEIGHT, 0, 0x0000FF});
 	mlx_put_image_to_window(fdf->mlx_p, fdf->win_p, fdf->img_p, 0, 0);
 	mlx_hook(fdf->win_p, 17, 0, close_win, fdf);
 	mlx_hook(fdf->win_p, 2, 1, ft_exit, fdf);
@@ -44,7 +42,6 @@ void	ft_initialize(t_map *map_data, char *filename, float theta)
 	assign_xy(map_data);
 	assign_z_color(filename, map_data);
 	map_data->ratio = get_ratio(map_data);
-	// map_data->ratio = 20;
 	dprintf(1, "ratio: %f\n", map_data->ratio);
 	apply_ratio(map_data);
 	apply_iso(map_data);
